2823cd81cb7167e7253b87ff0da75fa1b548bf6f
samuelstefancic <samuelstefancic@gmail.com>
2023-03-21 19:17:32 +0100

    Ajout services et composants angular

--- expense-tracker-frontend/src/app/app.module.ts
+++ expense-tracker-frontend/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './admin/components/user/user.component';
+import { ExpenseComponent } from './admin/components/expense/expense.component';
+
+//Http module
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    ExpenseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

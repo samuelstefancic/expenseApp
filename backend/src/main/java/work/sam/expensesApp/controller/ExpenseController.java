9a2e94d250087374ad1945b850d11e09078526f1
samuelstefancic <samuelstefancic@gmail.com>
2023-03-23 10:05:19 +0100

    Ajout méthodes CRUD UserController + début des méthodes controller ExpenseController

--- backend/src/main/java/work/sam/expensesApp/controller/ExpenseController.java
+++ backend/src/main/java/work/sam/expensesApp/controller/ExpenseController.java
@@ -1,15 +1,31 @@
 package work.sam.expensesApp.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import work.sam.expensesApp.entity.Expense;
 import work.sam.expensesApp.service.ExpenseService;
+import work.sam.expensesApp.service.UserService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/expenses")
 public class ExpenseController {
     private final ExpenseService expenseService;
+    private final UserService userService;
 
-    public ExpenseController(ExpenseService expenseService) {
+    public ExpenseController(ExpenseService expenseService, UserService userService) {
         this.expenseService = expenseService;
+        this.userService = userService;
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<Expense>> getExpensesByUserId(@PathVariable Long userId) {
+        userService.getUserById(userId);
+        List<Expense> expenses = expenseService.getExpensesByUserId(userId);
+        return ResponseEntity.ok(expenses);
     }
 }
--- backend/src/main/java/work/sam/expensesApp/controller/UserController.java
+++ backend/src/main/java/work/sam/expensesApp/controller/UserController.java
@@ -1,8 +1,11 @@
 package work.sam.expensesApp.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import work.sam.expensesApp.entity.User;
+import work.sam.expensesApp.service.ExpenseService;
 import work.sam.expensesApp.service.UserService;
 
 @RestController
@@ -15,4 +18,24 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUserById(@PathVariable Long userId) {
+        User user = userService.getUserById(userId);
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<User> updateUser(@PathVariable Long userId, @RequestBody @Valid User updatedUser) {
+        User user = userService.updateUser(userId, updatedUser);
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long userId) {
+        userService.deleteUserById(userId);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
